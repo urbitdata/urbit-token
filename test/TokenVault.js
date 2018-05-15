48b543c362a36c0f352fa602c5b07ad089a3b8b8
Chuck LeDuc Díaz <celeduc@gmail.com>
2018-05-15 23:01:04 +0200

    restore TokenVault.js, remove TokenVesting.js

---
+++ test/TokenVault.js
@@ -0,0 +1,28 @@
+/* eslint-env node, mocha */
+const BasicTokenMock = artifacts.require('../contracts/mocks/BasicTokenMock.sol');
+const BigNumber = require('bignumber.js');
+const expectThrow = require('./helpers/expectThrow.js');
+
+const should = require('chai') // eslint-disable-line no-unused-vars
+  .use(require('chai-as-promised'))
+  .use(require('chai-bignumber')(BigNumber))
+  .should();
+
+contract('TokenVault', (accounts) => {
+  const account1 = accounts[1];
+  const account2 = accounts[2];
+
+  context('vault', () => {
+    it('should fail to create an empty vault', async () => {
+      const tvMock = await BasicTokenMock.new(account1, 10000);
+      await tvMock.createTokenVault(0);
+      await expectThrow(tvMock.fillUpAllowance());
+    });
+
+    it('should create a regular vault', async () => {
+      const tvMock = await BasicTokenMock.new(account2, 10000);
+      await tvMock.createTokenVault(1000);
+      await tvMock.fillUpAllowance();
+    });
+  });
+});
