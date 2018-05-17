ca1516762bbd4f4c86f315031735ae323d5173d0
Chuck LeDuc Díaz <celeduc@gmail.com>
2018-05-17 16:01:41 +0200

    added tests for token sale

--- test/Lifecycle.js
+++ test/Lifecycle.js
@@ -1,6 +1,6 @@
 /* eslint-env node, mocha */
 const UrbitToken = artifacts.require('../contracts/UrbitToken.sol');
-// const expectThrow = require('./helpers/expectThrow.js');
+const expectThrow = require('./helpers/expectThrow.js');
 const BigNumber = require('bignumber.js');
 
 const should = require('chai') // eslint-disable-line no-unused-vars
@@ -9,17 +9,19 @@ const should = require('chai') // eslint-disable-line no-unused-vars
   .should();
 
 contract('Urbit', (accounts) => {
-  // const creator = accounts[0];
+  const creator = accounts[0];
   const admin = accounts[1];
   const bonus = accounts[2];
   const sale = accounts[3];
   const referral = accounts[4];
-  // const alix = accounts[5];
-  // const barb = accounts[6];
+  const alix = accounts[5];
+  const barb = accounts[6];
   // const carl = accounts[7];
   // const doug = accounts[8];
   // const earl = accounts[9];
   var urbitToken; // eslint-disable-line no-var
+
+  const presaleAmount = 1000;
 
   before(async () => {
     urbitToken = await UrbitToken.new(admin, bonus, sale, referral);
@@ -27,8 +29,27 @@ contract('Urbit', (accounts) => {
 
   context('pre-sale', () => {
     it('should send tokens to pre-sale buyers', async () => {
-      // send tokens to alix, barb
-      //  await expectThrow(UrbitToken.new(0, bonus, sale, referral));
+      // send tokens to alix
+      let result = await urbitToken.transfer(alix, presaleAmount, { from: sale });
+      result.logs[0].event.should.be.eq('Transfer');
+      // verify balance
+      (await urbitToken.balanceOf(alix)).toNumber().should.be.eq(presaleAmount);
+      // send tokens to barb
+      result = await urbitToken.transfer(barb, presaleAmount, { from: sale });
+      result.logs[0].event.should.be.eq('Transfer');
+      // verify balance
+      (await urbitToken.balanceOf(barb)).toNumber().should.be.eq(presaleAmount);
+    });
+
+    it('should not allow pre-sale buyers to transfer their tokens', async () => {
+      result = await urbitToken.transfer(barb, 100, { from: alix });
+      // no event emitted
+      result.logs.length.should.eq(0);
+      // balance remains the same
+      (await urbitToken.balanceOf(alix)).toNumber().should.be.eq(presaleAmount);
+      result = await urbitToken.transfer(creator, 100, { from: barb });
+      result.logs.length.should.eq(0);
+      (await urbitToken.balanceOf(barb)).toNumber().should.be.eq(presaleAmount);
     });
   });
 
@@ -38,7 +59,7 @@ contract('Urbit', (accounts) => {
     });
 
     it('should not have increased the total supply beyond the hard cap', async () => {
-      //  (await urbitToken.HARD_CAP()).should.be.bignumber.eq(await urbitToken.totalSupply());
+      (await urbitToken.HARD_CAP()).should.be.bignumber.eq(await urbitToken.totalSupply());
     });
   });
 
@@ -51,6 +72,9 @@ contract('Urbit', (accounts) => {
   });
 
   context('vest tokens', () => {
+    it('should lock tokens', async () => {
+      //  (await urbitToken.lockedBalanceOf(alice)).toNumber().should.be.equal(0);
+    });
     it('should get the locked balance for an owner', async () => {
       //  (await urbitToken.lockedBalanceOf(alice)).toNumber().should.be.equal(0);
     });
