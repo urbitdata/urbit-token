89697956b553f46288b084f5bf49525435551f54
Chuck LeDuc Díaz <celeduc@gmail.com>
2018-05-28 16:12:52 +0200

    use TokenVesting.js as template for PresaleTokenVesting.js

---
+++ test/PresaleTokenVesting.js
@@ -0,0 +1,61 @@
+/* eslint-env node, mocha */
+const latestTime = require('./helpers/latest-time');
+const { increaseTimeTo, duration } = require('./helpers/increase-time');
+
+const { BigNumber } = web3;
+
+require('chai')
+  .use(require('chai-as-promised'))
+  .use(require('chai-bignumber')(BigNumber))
+  .should();
+
+const UrbitToken = artifacts.require('UrbitToken');
+const TokenVesting = artifacts.require('TokenVesting');
+
+contract('PresaleTokenVesting', ([admin, sale, beneficiary]) => {
+  let MAGNITUDE;
+  let bonusTokensVault;
+  let bonusTokensVaultAmount;
+  const ZERO = new BigNumber(0);
+  const vestedAmount = new BigNumber(1000);
+
+  beforeEach(async () => {
+    this.token = await UrbitToken.new(admin, sale);
+    await this.token.createSaleTokens({ from: admin });
+
+    this.start = latestTime() + duration.minutes(1); // +1 minute so it starts after contract instantiation
+    this.duration = duration.years(2);
+
+    await this.token.closeSale({ from: admin });
+
+    // hardcoded in the contract
+    MAGNITUDE = await this.token.MAGNITUDE();
+    bonusTokensVault = await this.token.bonusTokensVault();
+    bonusTokensVaultAmount = MAGNITUDE.mul(new BigNumber(41346823));
+  });
+
+  it('should lock some balance equal to the hardcoded amount', async () => {
+    const balanceOfVault = await this.token.balanceOf(bonusTokensVault);
+    balanceOfVault.should.bignumber.equal(bonusTokensVaultAmount);
+  });
+
+  it('should lock balance before start', async () => {
+    await this.token.lockTokens(bonusTokensVault, vestedAmount, beneficiary, this.start);
+    (await this.token.lockedBalanceOf(beneficiary)).should.bignumber.equal(vestedAmount);
+    (await this.token.releasableBalanceOf(beneficiary)).should.bignumber.equal(ZERO);
+    (await this.token.balanceOf(beneficiary)).should.bignumber.equal(ZERO);
+  });
+
+  it('should have released all after end', async () => {
+    await this.token.lockTokens(bonusTokensVault, vestedAmount, beneficiary, this.start);
+    await increaseTimeTo(this.start + duration.minutes(1));
+    (await this.token.balanceOf(beneficiary)).should.bignumber.equal(ZERO);
+    (await this.token.releasableBalanceOf(beneficiary)).should.bignumber.equal(vestedAmount);
+    await (await new TokenVesting(await this.token.vestingOf(beneficiary))).release(this.token.address);
+    (await this.token.balanceOf(beneficiary)).should.bignumber.equal(vestedAmount);
+  });
+  it('should handle multiple grants to the same beneficiary', async () => {
+    await this.token.lockTokens(bonusTokensVault, vestedAmount, beneficiary, this.start);
+    await this.token.lockTokens(bonusTokensVault, vestedAmount, beneficiary, this.start);
+  });
+});
