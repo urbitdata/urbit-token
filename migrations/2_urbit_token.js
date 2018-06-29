151e02796856055620106229af1e3d209b53a75c
Chuck LeDuc Díaz <celeduc@gmail.com>
2018-06-29 19:50:05 +0200

    update admin and sale address for Ropsten

--- migrations/2_urbit_token.js
+++ migrations/2_urbit_token.js
@@ -2,6 +2,6 @@ const UrbitToken = artifacts.require('UrbitToken');
 
 module.exports = function (deployer) { // eslint-disable-line func-names
   // Create UrbitToken, passing in the Urbit Admin address and Sales address
-  // TODO: use real addresses
-  deployer.deploy(UrbitToken, '0xf17f52151EbEF6C7334FAD080c5704D77216b732', '0xC5fdf4076b8F3A5357c5E395ab970B5B54098Fef');
+  // TODO: these are Ropsten test addresses, use real addresses
+  deployer.deploy(UrbitToken, '0x935d3be8136E04B843530a2359148385Ee87bEEb', '0x55d43e43959b2dd37ec36661d9d0d878414b0908');
 };
