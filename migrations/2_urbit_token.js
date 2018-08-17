fc570e0ea7722ed7174a0e45cc6351768b5e8357
Chuck LeDuc Díaz <celeduc@gmail.com>
2018-08-17 11:46:53 +0200

    updated with sales address for mainnet

--- migrations/2_urbit_token.js
+++ migrations/2_urbit_token.js
@@ -3,5 +3,5 @@ const UrbitToken = artifacts.require('UrbitToken');
 module.exports = function (deployer) { // eslint-disable-line func-names
   // Create UrbitToken, passing in the Urbit Admin address and Sales address
   // TODO: these are Ropsten test addresses, use real addresses
-  deployer.deploy(UrbitToken, '0x935d3be8136E04B843530a2359148385Ee87bEEb', '0x55d43e43959b2dd37ec36661d9d0d878414b0908');
+  deployer.deploy(UrbitToken, '0x935d3be8136E04B843530a2359148385Ee87bEEb', '0x9f8ac07005E8347271d326ab50418Bfb017506Bc');
 };
