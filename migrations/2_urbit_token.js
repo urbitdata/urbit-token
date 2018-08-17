f4a0418965e2d83221f5d0132a266b9e4fa45820
Chuck LeDuc Díaz <celeduc@gmail.com>
2018-08-17 12:34:37 +0200

    change admin address for mainnet

--- migrations/2_urbit_token.js
+++ migrations/2_urbit_token.js
@@ -3,5 +3,5 @@ const UrbitToken = artifacts.require('UrbitToken');
 module.exports = function (deployer) { // eslint-disable-line func-names
   // Create UrbitToken, passing in the Urbit Admin address and Sales address
   // TODO: these are Ropsten test addresses, use real addresses
-  deployer.deploy(UrbitToken, '0x935d3be8136E04B843530a2359148385Ee87bEEb', '0x9f8ac07005E8347271d326ab50418Bfb017506Bc');
+  deployer.deploy(UrbitToken, '0xE39F321390c7C1F4b3BC182D88Cf78db4B3A02a1', '0x9f8ac07005E8347271d326ab50418Bfb017506Bc');
 };
